7dc77fcc8bcf646c76eb5adb2f70226283aff9e9
WojciechWnukk <wojciech.wnukk@gmail.com>
2024-11-08 15:54:48 +0100

    Test form, CR without UD

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,18 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../services/api.service';
 import { get } from 'http';
+import { FormsModule } from '@angular/forms';
+
+interface Product {
+  name: string;
+  price: number;
+  description: string;
+}
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-
   //get products from service
   products: any[] = [];
+  product: Product = {
+    name: '',
+    price: 0,
+    description: '',
+  };
 
   constructor(private apiService: ApiService) {}
 
@@ -23,6 +34,21 @@ export class ProductListComponent implements OnInit {
     });
   }
 
-
-
+  //add product
+  addProduct() {
+    this.apiService.addProduct(this.product).subscribe({
+      next: (response) => {
+        this.products.push(response);
+        // Reset form
+        this.product = {
+          name: '',
+          price: 0,
+          description: '',
+        };
+      },
+      error: (error) => {
+        console.error('Error adding product:', error);
+      },
+    });
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   private apiUrl = 'http://127.0.0.1:8000/api';
@@ -12,5 +12,10 @@ export class ApiService {
 
   getProducts(): Observable<any> {
     return this.http.get(`${this.apiUrl}/products`);
+  }
+
+  addProduct(product: any): Observable<any> {
+    console.log(product);
+    return this.http.post(`${this.apiUrl}/products`, product);
   }
 }
